80bafb60c9c765e5a4358d0d7bf212107b153985
lubelle <belleliullh@gmail.com>
2018-12-16 20:13:28 -0500

    setup project structure

---
+++ material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule,
+         MatIconModule,
+         MatInputModule,
+         MatFormFieldModule } from '@angular/material';
+
+@NgModule({
+    imports: [MatButtonModule,
+              MatIconModule,
+              MatInputModule,
+              MatFormFieldModule],
+    exports: [MatButtonModule,
+              MatIconModule,
+              MatInputModule,
+              MatFormFieldModule]
+})
+export class MaterialModule {
+
+}
